4b6e8a8a53ff512c437b7ac22a625469613a2c58
Alexandr <alexandr.leder@yandex.ru>
2022-05-30 14:31:42 +0300

    task16

---
+++ Task16/Domain/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Task16.Domain;
+
+public class Product
+{
+    public virtual int Id { get; set; }
+    public virtual string Name { get; set; }
+}
---
+++ Task16/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Task16.Domain;
+
+namespace Task16.Repositories;
+
+public interface IProductRepository
+{
+    void Add(Product product);
+    void Update(Product product);
+    void Remove(Product product);
+
+    Product GetByID(int id);
+    Product GetByName(string name);
+}
